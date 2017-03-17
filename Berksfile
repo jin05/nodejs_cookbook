c9ea9912aba115612e614a163b0f861b624f55d7
Jin Makino <jin.makino@translimit.co.jp>
2017-03-17 11:38:05 +0900

    first commit

---
+++ Berksfile
@@ -0,0 +1,9 @@
+source 'https://supermarket.chef.io/'
+
+cookbook 'apt', '= 3.0.0'
+cookbook 'build-essential', '= 3.2.0'
+cookbook 'homebrew', '= 2.1.2'
+cookbook 'nodejs', '~> 2.4.4'
+cookbook 'ohai', '= 3.0.1'
+cookbook 'yum', '= 3.13.0'
+cookbook 'yum-epel', '= 0.7.1'
---
+++ custom/attributes/default.rb
@@ -0,0 +1,8 @@
+### Node.js ###
+
+default['nodejs']['engine'] = 'node'
+default['nodejs']['version'] = '6.9.5'
+default['nodejs']['install_method'] = 'binary'
+
+# See `http://nodejs.org/dist/v5.5.0/SHASUMS256.txt`
+override['nodejs']['binary']['checksum'] = 'f7e9ab702c5d1f5a3521199c04cc670fda3cf4b0e48548b09ac7ac874ccb504a'
